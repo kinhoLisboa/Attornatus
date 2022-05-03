b29f872d02e9179264576fbda5d39009e3d7b183
kinho Lisboa <danifleikson@hotmail.com>
2022-05-03 19:33:06 -0300

    criando tabela vendedor

---
+++ projetoTeste/src/main/resources/db/migration/V001__cria-tabela-vendedor.sql
@@ -0,0 +1,10 @@
+create table vendedor (
+
+id bigint not null auto_increment,
+nome varchar(60) not null,
+totalVendas varchar(60) not null,
+mediaVendasDiaria varchar (60) not null,
+
+primary key(id)
+
+);
