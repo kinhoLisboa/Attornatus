8d66cc1972d33aa1abe7a79faa5e664394b0865a
kinho Lisboa <danifleikson@hotmail.com>
2022-05-04 12:58:02 -0300

    criando projeto de teste e adicionando dependecias

---
+++ xbrainTeste/src/main/java/com/xbrain/teste/XbrainTesteApplication.java
@@ -0,0 +1,13 @@
+package com.xbrain.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrainTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrainTesteApplication.class, args);
+	}
+
+}
---
+++ xbrainTeste/src/test/java/com/xbrain/teste/XbrainTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbrain.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbrainTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
