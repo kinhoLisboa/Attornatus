6f40612f554d629f4630efd14a8bc71e024a55c5
kinho Lisboa <danifleikson@hotmail.com>
2022-05-05 20:47:42 -0300

    Criando projeto de teste

---
+++ testeVendas/src/main/java/com/teste/vendas/TesteVendasApplication.java
@@ -0,0 +1,13 @@
+package com.teste.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteVendasApplication.class, args);
+	}
+
+}
---
+++ testeVendas/src/test/java/com/teste/vendas/TesteVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
