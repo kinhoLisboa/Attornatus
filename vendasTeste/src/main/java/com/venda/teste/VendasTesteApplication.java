48d653a1ec4d5059d966a02d32755cc723277b96
kinho Lisboa <danifleikson@hotmail.com>
2022-05-05 12:50:12 -0300

    criando o projeto e adicionando dependencias

---
+++ vendasTeste/src/main/java/com/venda/teste/VendasTesteApplication.java
@@ -0,0 +1,13 @@
+package com.venda.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasTesteApplication.class, args);
+	}
+
+}
---
+++ vendasTeste/src/test/java/com/venda/teste/VendasTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venda.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
