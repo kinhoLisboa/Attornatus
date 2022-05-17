d33a9b0c1bdc356bae5288f26e0216eae428360e
kinho Lisboa <danifleikson@hotmail.com>
2022-05-17 19:01:57 -0300

    Implementando o crud de funcionarios.

--- MeuTeste/src/main/java/com/meu/teste/controller/FuncionarioController.java
+++ MeuTeste/src/main/java/com/meu/teste/controller/FuncionarioController.java
@@ -2,7 +2,16 @@ package com.meu.teste.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.meu.teste.modelo.Funcionario;
@@ -22,6 +31,40 @@ public class FuncionarioController {
 		return funcionarioRepository.findAll();
 		
 	}
+	
+	@GetMapping("/funcionarios/{funcionarioId}")
+	public ResponseEntity<Funcionario> buscar (@PathVariable Long funcionarioId){
+		return funcionarioRepository.findById(funcionarioId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+				
+	}
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("/funcionarios")
+	public Funcionario adicionar (@RequestBody Funcionario funcionario) {
+		return funcionarioRepository.save(funcionario);
+	}
+	
+	@PutMapping("funcionarios/{funcionarioId}")
+	public ResponseEntity<Funcionario> atualizar (@PathVariable Long funcionarioId,@RequestBody Funcionario funcionario){
+		if(!funcionarioRepository.existsById(funcionarioId)) {
+			return ResponseEntity.notFound().build();
+		}
+		funcionario.setId(funcionarioId);
+		funcionarioRepository.save(funcionario);
+		return ResponseEntity.ok(funcionario);
+	}
+	
+	@DeleteMapping("/funcionarios/{funcionarioId}")
+	public ResponseEntity<Void> excluir(@PathVariable Long funcionarioId){
+		if(!funcionarioRepository.existsById(funcionarioId)) {
+			return ResponseEntity.notFound().build();
+		}
+		funcionarioRepository.deleteById(funcionarioId);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 	
 	
 }
