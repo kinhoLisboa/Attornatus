19661da96a8a1e8eff395c712d95a9f0403639f4
kinho Lisboa <danifleikson@hotmail.com>
2022-05-16 21:09:07 -0300

    Criando o projeto do meu teste do processo seletivo

---
+++ MeuTeste/src/main/java/com/meu/teste/MeuTesteApplication.java
@@ -0,0 +1,13 @@
+package com.meu.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuTesteApplication.class, args);
+	}
+
+}
---
+++ MeuTeste/src/test/java/com/meu/teste/MeuTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meu.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
