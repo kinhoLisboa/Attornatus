de0f620332a211e25b52a38f4781cc95fa9345c0
kinho Lisboa <danifleikson@hotmail.com>
2022-05-18 19:43:29 -0300

    Trabalhando com minha classe controller.

--- MeuTeste/src/main/java/com/meu/teste/controller/FuncionarioController.java
+++ MeuTeste/src/main/java/com/meu/teste/controller/FuncionarioController.java
@@ -3,14 +3,16 @@ package com.meu.teste.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.HeadersBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -64,7 +66,15 @@ public class FuncionarioController {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping("/funcionarios/{funcionarioId}")
+	public ResponseEntity<Funcionario> atualizarPorDado (@PathVariable Long funcionarioId,@RequestBody Funcionario funcionario){
+		if(!funcionarioRepository.existsById(funcionarioId)) {
+			return ResponseEntity.notFound().build();
+		}
+		funcionario.setId(funcionarioId);
+		funcionarioRepository.save(funcionario);
+		return ResponseEntity.ok(funcionario);
 	
 	
-	
+	}
 }
